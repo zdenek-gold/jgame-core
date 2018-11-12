d4cc0e2b1f567ba3c6ee330b28e45f409e100ff5
Zigi <zigi@zdenek-gold.cz>
2018-11-12 10:04:19 +0100

    Player components #2

--- src/main/java/org/zigi/game/jgamecore/player/Player.java
+++ src/main/java/org/zigi/game/jgamecore/player/Player.java
@@ -1,9 +1,30 @@
 package org.zigi.game.jgamecore.player;
 
-public abstract class Player implements IPlayer {
-	protected String name;
+public class Player {
+	private static int playerIndex = 1;
+	private String name;
+
+	public Player() {
+		this.name = getRandomName();
+	}
+
+	public Player(String name) {
+		this.name = name;
+	}
 
 	public String getName() {
-		return "";
+		return name;
+	}
+
+	private static String getRandomName() {
+		return "Player" + playerIndex++;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isValid() {
+		return false;
 	}
 }
--- src/main/java/org/zigi/game/jgamecore/player/PlayerCollection.java
+++ src/main/java/org/zigi/game/jgamecore/player/PlayerCollection.java
@@ -1,75 +1,130 @@
 package org.zigi.game.jgamecore.player;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
-public class PlayerCollection {
+public class PlayerCollection<T extends Player> implements Collection<T> {
 
-	private List<Player> players = new ArrayList<Player>();
-	private Player active = null;
+	private List<T> players = new ArrayList<T>();
+	private int activeIndex = 0;
 
-	private PlayerCollection() {
+	public PlayerCollection() {
 
 	}
 
-	private String getRandomName() {
-		return "Player" + players.size() + 1;
+	public Integer getOrder(Player player) {
+		if (players.contains(player))
+			return players.indexOf(player);
+		return null;
 	}
 
-	public static PlayerCollection getInstance() {
-		return new PlayerCollection();
+	private int getActiveIndex() {
+		return activeIndex;
 	}
 
-	public void addPlayer() {
-		players.add(new Player());
-	}
-
-	public void removePlayer(Player p) {
-		if (p == null || !players.contains(p))
-			return;
-		// TODO doplnit chování
-	}
-
-	public Player nextPlayer() {
-		Player p = getActive();
-		if (p == null)
+	public T nextActive() {
+		if (players.isEmpty())
 			return null;
 
-		int idx = players.indexOf(p);
-		idx = (idx + 1) % players.size();
-		return players.get(idx);
+		activeIndex = (getActiveIndex() + 1) % players.size();
+		return getActive();
 	}
 
-	public Player getActive() {
-		if (active == null && !players.isEmpty())
-			active = players.get(0);
-		return active;
-
-	}
-
-	public class Player {
-		private String name;
-
-		public Player() {
-			this.name = getRandomName();
-		}
-
-		public Player(String name) {
-			this.name = name;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public Integer getOrder() {
-			if (players.contains(this))
-				return players.indexOf(this);
+	public T getActive() {
+		if (players.isEmpty())
 			return null;
+		return players.get(activeIndex);
+	}
+
+	public boolean add(T player) {
+		if (players.contains(player))
+			return false;
+		players.add(player);
+		return true;
+	}
+
+	public boolean addAll(Collection<? extends T> player) {
+		if (player == null)
+			return false;
+
+		boolean isValid = true;
+		for (T p : player) {
+			isValid &= add(p);
 		}
+		return isValid;
+	}
+
+	public void clear() {
+		players.clear();
+		activeIndex = 0;
+	}
+
+	public boolean contains(Object player) {
+		return players.contains(player);
+	}
+
+	public boolean containsAll(Collection<?> player) {
+		for (Object o : player)
+			if (!players.contains(o))
+				return false;
+		return true;
+	}
+
+	public boolean isEmpty() {
+		return players.isEmpty();
+	}
+
+	public Iterator<T> iterator() {
+		return players.iterator();
+	}
+
+	public boolean removeAll(Collection<?> players) {
+		if (players == null)
+			return false;
+
+		boolean isValid = true;
+		for (Object player : players) {
+			isValid &= remove(player);
+		}
+		return isValid;
+	}
+
+	public boolean retainAll(Collection<?> player) {
+		return players.retainAll(player);
+	}
+
+	public int size() {
+		return players.size();
+	}
+
+	public Object[] toArray() {
+		return players.toArray();
+	}
+
+	@SuppressWarnings("hiding")
+	public <T> T[] toArray(T[] player) {
+		return players.toArray(player);
+	}
+
+	public boolean remove(Object player) {
+		if (player == null || !players.contains(player))
+			return false;
+
+		int index = players.indexOf(player);
+		if (players.size() == 1) {
+			clear();
+		} else if (index < activeIndex) {
+			activeIndex -= 1;
+			players.remove(player);
+		} else if (index > activeIndex) {
+			players.remove(player);
+		} else {
+			T next = players.get((activeIndex + 1) % players.size());
+			players.remove(player);
+			activeIndex = players.indexOf(next);
+		}
+		return true;
 	}
 }
